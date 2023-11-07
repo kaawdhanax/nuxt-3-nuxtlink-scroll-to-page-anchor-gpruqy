191e702280c2aa3cdc5cb914069925e0c520f78d
kaawdhanax <61614236+kaawdhanax@users.noreply.github.com>
2023-11-07 15:01:42 +0700

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1 id="top">Scroll allllll the way down</h1>
+    <div v-for="i in 100" :key="i" class="filler">Filler Div</div>
+    <div>
+      <NuxtLink to="#top">
+        <button>To top with NuxtLink to="#top"</button>
+      </NuxtLink>
+    </div>
+    <div>
+      <NuxtLink to="/">
+        <button>Click to test scroll to top on same page</button>
+      </NuxtLink>
+    </div>
+  </div>
+</template>
